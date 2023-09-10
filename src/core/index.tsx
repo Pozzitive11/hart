1350ba59f0bbcbe58d3ed7f0a94a6f5ade961e18
Palahin Vladyslav <palaga2442@gmail.com>
2023-09-10 10:50:04 +0300

    feat(HomePage): create a new homepage component

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,11 +1,14 @@
 import { Layout } from "modules/layout";
+import { HomePage } from "pages/home-page/ui";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
